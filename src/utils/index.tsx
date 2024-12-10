dc9ad2c4a6ae6afb72ed2fc2776bfb38854f4640
CookieSan <chibakenta123@gmail.com>
2024-12-10 18:27:13 +0700

    Update index.tsx

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -50,6 +50,10 @@ export const getSanitizedConfig = (
             projects: config?.projects?.github?.manual?.projects || [],
           },
         },
+        active: {
+          header: config?.projects?.external?.header || 'My Projects',
+          projects: config?.projects?.external?.projects || [],
+        },
         external: {
           header: config?.projects?.external?.header || 'My Projects',
           projects: config?.projects?.external?.projects || [],
